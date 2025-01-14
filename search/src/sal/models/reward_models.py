21a31f0b83e7ab9b2c15c289df64e88511e7f84e
cjcjcjy <3328671693@qq.com>
2025-01-14 13:57:43 +0800

    xiugai

--- search/src/sal/models/reward_models.py
+++ search/src/sal/models/reward_models.py
@@ -168,6 +168,168 @@ class RLHFFlow(PRM):
     def load_model_and_tokenizer(
         self, **model_kwargs
     ) -> tuple[PreTrainedModel, PreTrainedTokenizer]:
+        tokenizer = AutoTokenizer.from_pretrained(
+            "RLHFlow/Llama3.1-8B-PRM-Deepseek-Data"
+        )
+        model = AutoModelForCausalLM.from_pretrained(
+            "RLHFlow/Llama3.1-8B-PRM-Deepseek-Data",
+            device_map="auto",
+            torch_dtype=torch.bfloat16,
+            **model_kwargs,
+        ).eval()
+        tokenizer.padding_side = "right"
+        tokenizer.pad_token = tokenizer.eos_token
+        model.config.pad_token_id = model.config.eos_token_id
+
+        plus_tag_id = tokenizer.encode("+")[-1]
+        minus_tag_id = tokenizer.encode("-")[-1]
+        self.candidate_tokens = [plus_tag_id, minus_tag_id]
+
+        return model, tokenizer
+
+    def score(
+        self,
+        questions: list[str],
+        outputs: list[list[str]],
+        batched: bool = True,
+        batch_size=8,
+    ) -> list[list[float]]:
+        if batched is True:
+            return self._score_batched(questions, outputs, batch_size=batch_size)
+        else:
+            return self._score_single(questions, outputs)
+
+    def _score_single(self, questions: list[str], outputs: list[list[str]]):
+        # reference code: https://github.com/RLHFlow/RLHF-Reward-Modeling/blob/main/math-rm/prm_evaluate.py
+        all_scores = []
+        for question, answers in zip(questions, outputs, strict=True):
+            all_step_scores = []
+            for ans in answers:
+                single_step_score = []
+                conversation = []
+                ans_list = ans.split("\n\n")
+                for k in range(len(ans_list)):
+                    if k == 0:
+                        # TODO: add the system prompt like we did for math shepard?
+                        text = question + " " + ans_list[0]
+                    else:
+                        text = ans_list[k]
+                    conversation.append({"content": text, "role": "user"})
+                    conversation.append({"content": "+", "role": "assistant"})
+                    input_ids = self.tokenizer.apply_chat_template(
+                        conversation, return_tensors="pt"
+                    ).to(self.model.device)
+                    with torch.no_grad():
+                        logits = self.model(input_ids).logits[
+                            :, -3, self.candidate_tokens
+                        ]  # simple version, the +/- is predicted by the '-3' position
+                        step_scores = logits.softmax(dim=-1)[
+                            :, 0
+                        ]  # 0 means the prob of + (1 mean -)
+                        # print(scores)
+                        single_step_score.append(
+                            step_scores[0]
+                            .detach()
+                            .to("cpu", dtype=torch.float32)
+                            .item()
+                        )
+
+                all_step_scores.append(single_step_score)
+            all_scores.append(all_step_scores)
+        return all_scores
+
+    def _score_batched(
+        self, questions: list[str], outputs: list[list[str]], batch_size: int = 2
+    ):
+        # The RLHFlow models are trained to predict the "+" or "-" tokens in a dialogue, but since these are not unique
+        # we need to introduce a dummy special token here for masking.
+
+        special_tok_id = self.tokenizer("ки", return_tensors="pt").input_ids[0, 1]
+        # We construct two parallel dialogues, one with a "+" token per assistant turn, the other with the dummy token "ки" for masking
+        conversations = []
+        conversations2 = []
+        for question, answers in zip(questions, outputs, strict=True):
+            for ans in answers:
+                conversation = []
+                conversation2 = []
+                ans_list = ans.split("\n\n")
+                for k in range(len(ans_list)):
+                    if k == 0:
+                        text = question + " " + ans_list[0]
+                    else:
+                        text = ans_list[k]
+                    conversation.append({"content": text, "role": "user"})
+                    conversation.append({"content": "+", "role": "assistant"})
+
+                    # we track to location of the special token with ки in order to extract the scores
+                    conversation2.append({"content": text, "role": "user"})
+                    conversation2.append({"content": "ки", "role": "assistant"})
+
+                conversations.append(conversation)
+                conversations2.append(conversation2)
+
+        output_scores = []
+        for i in range(0, len(conversations), batch_size):
+            convs_batch = conversations[i : i + batch_size]
+            convs2_batch = conversations2[i : i + batch_size]
+            inputs_batch = self.tokenizer.apply_chat_template(
+                convs_batch, padding=True, return_tensors="pt"
+            ).to(self.model.device)
+            inputs2_batch = self.tokenizer.apply_chat_template(
+                convs2_batch, padding=True, return_tensors="pt"
+            ).to(self.model.device)
+            assert inputs_batch.shape == inputs2_batch.shape
+            with torch.no_grad():
+                logits = self.model(inputs_batch).logits[:, :, self.candidate_tokens]
+                scores = logits.softmax(dim=-1)[
+                    :, :, 0
+                ]  # 0 means the prob of + (1 mean -)
+
+                for i in range(len(convs_batch)):
+                    # We slice on the N-1 token since the model is trained to predict the Nth one ("+" in this case)
+                    step_scores_flat = scores[i, :-1][
+                        inputs2_batch[i, 1:] == special_tok_id
+                    ].tolist()
+                    output_scores.append(step_scores_flat)
+
+        # reshape the output scores to match the input
+        reshaped_output_scores = []
+        counter = 0
+        for question, answers in zip(questions, outputs):
+            scores = []
+            for answer in answers:
+                scores.append(output_scores[counter])
+                counter += 1
+            reshaped_output_scores.append(scores)
+
+        return reshaped_output_scores
+
+
+def load_prm(config: Config) -> PRM:
+    if config.prm_path == "peiyi9979/math-shepherd-mistral-7b-prm":
+        return MathShepherd(config)
+
+    if config.prm_path == "RLHFlow/Llama3.1-8B-PRM-Deepseek-Data":
+        return RLHFFlow(config)
+    
+    if config.prm_path == "openreasoner/Math-psa":
+        return Math_psa(config)
+
+    raise NotImplementedError(f"PRM {config.prm_path} not implemented")
+
+
+from transformers import AutoTokenizer
+from transformers import AutoModelForCausalLM
+import torch
+from torch import nn
+from peft import PeftModel
+import numpy as np
+
+
+class Math_psa(PRM):
+    def load_model_and_tokenizer(
+        self, **model_kwargs
+    ) -> tuple[PreTrainedModel, PreTrainedTokenizer]:
         # tokenizer = AutoTokenizer.from_pretrained(
         #     "RLHFlow/Llama3.1-8B-PRM-Deepseek-Data"
         #     # "openreasoner/Math-psa"
@@ -329,141 +491,3 @@ class RLHFFlow(PRM):
             reshaped_output_scores.append(scores)
 
         return reshaped_output_scores
-
-
-def load_prm(config: Config) -> PRM:
-    if config.prm_path == "peiyi9979/math-shepherd-mistral-7b-prm":
-        return MathShepherd(config)
-
-    if config.prm_path == "RLHFlow/Llama3.1-8B-PRM-Deepseek-Data":
-        return RLHFFlow(config)
-    
-    if config.prm_path == "openreasoner/Math-psa":
-        return RLHFFlow(config)
-
-    raise NotImplementedError(f"PRM {config.prm_path} not implemented")
-
-
-from transformers import AutoTokenizer
-from transformers import AutoModelForCausalLM
-import torch
-from torch import nn
-from peft import PeftModel
-import numpy as np
-
-
-class QwenProcessRM(nn.Module):
-
-    def __init__(self, search_config: Config, all_args):
-        super().__init__()
-        # self.model_name_or_path = all_args.prm_model_name_or_path
-        # self.prm_checkpoint_path = all_args.prm_checkpoint_path
-        # print(f"prm_base_model_path: {self.model_name_or_path}")
-        # print(f"prm_checkpoint_path: {self.prm_checkpoint_path}")
-        
-        self.search_config = search_config
-
-        self.good_token = '+'
-        self.bad_token = '-'
-        self.step_tag = '\n\n\n\n\n'
-
-        self.tokenizer = AutoTokenizer.from_pretrained("/data/disk2/jjxiao/Qwen/Qwen2.5-Math-7B-Instruct", add_eos_token=False, padding_side='left')
-        self.tokenizer.pad_token_id = 151655 # "<|image_pad|>"
-        self.candidate_tokens = self.tokenizer.encode(f" {self.good_token} {self.bad_token}") # [488, 481]
-        self.step_tag_id = self.tokenizer.encode(f" {self.step_tag}")[-1] # 76325
-        self.model = AutoModelForCausalLM.from_pretrained("/data/disk2/jjxiao/Qwen/Qwen2.5-Math-1.5B-Instruct", 
-                                                          device_map="auto", 
-                                                          torch_dtype=torch.bfloat16,
-                                                        #   attn_implementation="flash_attention_2",
-                                                          ).eval()
-        # adapter_config = PeftConfig.from_pretrained(cp_path)
-        self.model = PeftModel.from_pretrained(self.model, "/data/disk2/jjxiao/checkpoint-2127/")
-
-    @torch.no_grad()
-    def get_reward(self, obs: list[np.ndarray[str]], actions: list[np.ndarray[str]]):
-        inputs_for_prm = []
-        for o, a in zip(obs.copy(), actions.copy()):
-            o = o[0].replace(IN_CONTEXT_EXAMPLE, "")
-            o = o.replace("ки", self.step_tag + " ")
-            a = a[0].replace("ки", "").strip()
-            inputs_for_prm.append(f"{o}{a} {self.step_tag}")
-        input_ids = self.tokenizer(inputs_for_prm, return_tensors="pt", padding=True).to("cuda")
-        logits = self.model(**input_ids).logits[:, :, self.candidate_tokens]
-        score = logits.softmax(dim=-1)[:, :, 0]
-        
-        step_scores = []
-        for i in range(np.shape(score)[0]):
-            step_score = score[i][input_ids["input_ids"][i] == self.step_tag_id]
-            last_step_score = step_score[-1]
-            step_scores.append([last_step_score.item()])
-        step_scores = np.array(step_scores)
-        
-        return step_scores
-    
-    def score(
-        self, questions: list[str], outputs: list[list[str]]
-    ) -> list[list[float]]:
-        inputs_for_prm = []
-        lengths = []
-        for question, output in zip(questions, outputs):
-            prompt = self.search_config.system_prompt + "\n" + question + "\n"
-            special_outputs = [o.replace("\n\n", " ки\n\n") for o in output]
-            special_outputs = [
-                o + " ки" if o[-2:] != "\n\n" else o for o in special_outputs
-            ]
-            inputs_for_prm.extend([f"{prompt} {o}" for o in special_outputs])
-            lengths.append(len(output))
-
-        # TODO: tokenize each batch independently so there is less padding and faster inference
-        output_scores = self.batched_math_shepherd_inference_new(
-            self.model,
-            self.tokenizer,
-            inputs_for_prm,
-            self.search_config.prm_batch_size,
-        )
-        cumulative_lengths = list(accumulate(lengths))
-        # reshape the output scores to match the input
-        output_scores = [
-            output_scores[i:j]
-            for i, j in zip([0] + cumulative_lengths[:-1], cumulative_lengths)
-        ]
-
-        # stripped_output_scores = [] TODO: strip out the reward for previous steps
-        for output_score, output in zip(output_scores, outputs):
-            assert len(output_score) == len(
-                output
-            ), f"{len(output_score)} != {len(output)}"
-
-        return output_scores
-    def batched_math_shepherd_inference_new(
-        self,
-        model: PreTrainedModel,
-        tokenizer: PreTrainedTokenizer,
-        inputs: list[str],
-        batch_size: int,
-    ) -> list[list[float]]:
-        output_scores = []
-        for i in range(0, len(inputs), batch_size):
-            inputs_batch = inputs[i : i + batch_size]
-            inputs_batch = tokenizer(inputs_batch, padding=True, return_tensors="pt").to(
-                model.device
-            )
-            with torch.no_grad():
-                logits = model(**inputs_batch).logits[:, :, self.candidate_tokens]
-                scores = logits.softmax(dim=-1)[:, :, 0]
-                step_scores_flat = scores[inputs_batch.input_ids == STEP_TAG_ID].tolist()
-                # Split scores into sublist based on number of \n in the input
-                step_scores = []
-                counter = 0
-                for i in range(len(inputs_batch.input_ids)):
-                    count = inputs_batch.input_ids[i].tolist().count(STEP_TAG_ID)
-                    step_scores.append(step_scores_flat[counter : counter + count])
-                    counter += count
-
-            # Store the step scores for this batch
-            output_scores.extend(step_scores)
-
-            # Clear GPU memory
-            del inputs_batch, logits, scores
-            torch.cuda.empty_cache()
-            return output_scores
